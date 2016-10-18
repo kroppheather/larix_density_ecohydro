a29c61a647b7f1384488b7aa110450b53be6fbb6
kroppheather <kroppheather@gmail.com>
2016-10-18 16:22:45 -0400

    checked model fit

--- model_comp.r
+++ model_comp.r
@@ -17,3 +17,9 @@ plot(datF[,1],datF[,3], xlim=c(0,12.5), ylim=c(0,12.5), pch=19, xlab="observed R
 abline(0,1, lwd=2)
 abline(fitrs,lty=2, lwd=2)
 #r2=0.48
+#second version of stochastic Rtot
+fitrs2<-lm(datF[,4]~datF[,1])
+summary(fitrs2)
+plot(datF[,1],datF[,4], xlim=c(0,12.5), ylim=c(0,12.5), pch=19, xlab="observed Rbio", ylab="predicted Rbio") 
+abline(0,1, lwd=2)
+abline(fitrs2,lty=2, lwd=2)
