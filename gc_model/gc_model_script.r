fe85e3fe7f3c977fdbd1a99fa1bce83f03826bf5
kroppheather <kroppheather@gmail.com>
2017-10-10 15:05:46 -0400

    organized gc and paired with met

--- gc_model/gc_model_script.r
+++ gc_model/gc_model_script.r
@@ -16,10 +16,95 @@
 ###########################################################################
 
 
-#read in sapflow calculations
+#################################################################
+####read in sapflow data                                  #######
+#################################################################
 source("c:\\Users\\hkropp\\Documents\\GitHub\\larch_density_ecohydro\\sapflux_process.r")
 #libraries loaded from source
 #plyr, lubridate,caTools
 
+#################################################################
+####read in datafiles                                     #######
+#################################################################
 
-#read in sesnor information
+# airport pressure and precip data
+datAirP <- read.csv("c:\\Users\\hkropp\\Google Drive\\viperSensor\\airport\\airport.csv")
+
+#canopy rh and temperature
+datRH <- read.csv("c:\\Users\\hkropp\\Google Drive\\viperSensor\\met\\RH.VP4.csv")
+datTC <- read.csv("c:\\Users\\hkropp\\Google Drive\\viperSensor\\met\\TempC.VP4.csv")
+
+#PAR
+datPAR <- read.csv("c:\\Users\\hkropp\\Google Drive\\viperSensor\\met\\PAR.QSOS PAR.csv")
+
+#################################################################
+####organize met data                                     #######
+#################################################################
+#subset and match
+datLRHmet <- data.frame(datRH[datRH$site=="ld",1:3], RH=datRH$RH.VP4[datRH$site=="ld"])
+datLTCmet <- data.frame(datTC[datTC$site=="ld",1:3], Temp=datTC$TempC.VP4[datTC$site=="ld"])
+
+datHRHmet <- data.frame(datRH[datRH$site=="hd",1:3], RH=datRH$RH.VP4[datRH$site=="hd"])
+datHTCmet <- data.frame(datTC[datTC$site=="hd",1:3], Temp=datTC$TempC.VP4[datTC$site=="hd"])
+#join temp and RH
+datLmet <- join(datLRHmet, datLTCmet, by=c("doy","year","hour"),type="inner")
+datHmet <- join(datHRHmet, datHTCmet, by=c("doy","year","hour"),type="inner")
+
+#join into gc tables
+datgcL <- join(gc.L,datLmet, by=c("doy","year","hour"), type="left")
+datgcH <- join(gc.H,datHmet, by=c("doy","year","hour"), type="left")
+datgcL17 <- join(gc.L17,datLmet, by=c("doy","year","hour"), type="left")
+datgcH17 <- join(gc.H17,datHmet, by=c("doy","year","hour"), type="left")
+
+######calculate VPD##########
+#calculate saturated vapor pressure
+datLe.sat<-0.611*exp((17.502*datgcL$Temp)/(datgcL$Temp+240.97))
+datLe.sat17<-0.611*exp((17.502*datgcL17$Temp)/(datgcL17$Temp+240.97))
+datHe.sat17<-0.611*exp((17.502*datgcH17$Temp)/(datgcH17$Temp+240.97))
+datHe.sat<-0.611*exp((17.502*datgcH$Temp)/(datgcH$Temp+240.97))
+
+#calculate vapor pressure deficit
+#here rh is is in decimal form 
+datgcL$RHfix<-ifelse(datgcL$RH>=1,.999,datgcL$RH)
+datgcL17$RHfix<-ifelse(datgcL17$RH>=1,.999,datgcL17$RH)
+datgcH$RHfix<-ifelse(datgcH$RH>=1,.999,datgcH$RH)
+datgcH17$RHfix<-ifelse(datgcH17$RH>=1,.999,datgcH17$RH)
+
+#vpd
+datgcL$D<-(datLe.sat-(datgcL$RHfix*datLe.sat))
+datgcL17$D<-(datLe.sat17-(datgcL17$RHfix*datLe.sat17))
+datgcH17$D<-(datHe.sat17-(datgcH17$RHfix*datHe.sat17))
+datgcH$D<-(datHe.sat-(datgcH$RHfix*datHe.sat))
+
+#join PAR to the dataframes
+datPARL <- data.frame(doy=datPAR$doy[datPAR$site=="ld"], year=datPAR$year[datPAR$site=="ld"],
+						hour=datPAR$hour[datPAR$site=="ld"], PAR=datPAR$PAR.QSOS.Par[datPAR$site=="ld"])
+datPARH <- data.frame(doy=datPAR$doy[datPAR$site=="hd"], year=datPAR$year[datPAR$site=="hd"],
+						hour=datPAR$hour[datPAR$site=="hd"], PAR=datPAR$PAR.QSOS.Par[datPAR$site=="hd"])						
+#join PAR to gc
+datgcL <- join(datgcL, datPARL, by=c("doy", "year","hour"), type="inner")						
+datgcL17 <- join(datgcL17, datPARL, by=c("doy", "year","hour"), type="inner")					
+datgcH <- join(datgcH, datPARH, by=c("doy", "year","hour"), type="inner")
+datgcH17 <- join(datgcH17, datPARH, by=c("doy", "year","hour"), type="inner")
+
+#join precip data to check for filtering
+datPrecip <- data.frame(doy=datAirP$doy, year=datAirP$year,Pr.mm = datAirP$Pr.mm)
+
+datgcL <- join(datgcL, datPrecip, by=c("doy", "year"), type="left")						
+datgcL17 <- join(datgcL17, datPrecip, by=c("doy", "year"), type="left")					
+datgcH <- join(datgcH, datPrecip, by=c("doy", "year"), type="left")
+datgcH17 <- join(datgcH17, datPrecip, by=c("doy", "year"), type="left")
+
+#################################################################
+####gc filter                                             #######
+#################################################################
+#light more than 5 umol m-2 s-1 (to exclude nocturnal measurements that may include refilling)
+#D>= 0.6kPA (per Ewers and Oren 2000)
+#precip more than 1mm
+
+m.gcL <- datgcL[datgcL$D>=.6&datgcL$PAR>5&datgcL$Pr.mm<=1,]
+m.gcL17 <- datgcL17[datgcL17$D>=.6&datgcL17$PAR>5&datgcL17$Pr.mm<=1,]
+m.gcH <- datgcH[datgcH$D>=.6&datgcH$PAR>5&datgcH$Pr.mm<=1,]
+m.gcH17 <- datgcH17[datgcH17$D>=.6&datgcH17$PAR>5&datgcH17$Pr.mm<=1,]
+
+
