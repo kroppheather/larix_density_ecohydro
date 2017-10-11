4bc48fdc1141a74d227ff37ae39264cd7d3f2acf
kroppheather <kroppheather@gmail.com>
2017-10-11 13:58:03 -0400

    started first model run and started soil data

--- gc_model/gc_model_code.r
+++ gc_model/gc_model_code.r
@@ -22,8 +22,7 @@ model{
 	gs[i]~dnorm(mu.gs[i],tau.gs)
 	#gs.rep[i]~dnorm(mu.gs[i],tau.gs)
 	#model for mean gs
-	mu.gs[i]<-oren.mod[i]
-	#*light[i]
+	mu.gs[i]<-oren.mod[i]*light[i]
 
 	#light scaling function
 	light[i]<-1-exp(-l.slope[standDay[i]]*PAR[i])
@@ -33,54 +32,61 @@ model{
 
 	}
 	for(i in 1:NstandDay){
-		gref[i]<-a1[stand[i]]+a2[stand[i]]*airTcent[i]+a3[stand[i]]*PrecipTot[i]
-		S[i]<-b1[stand[i]]+b2[stand[i]]*airTcent[stand[i]]+b3[stand[i]]*PrecipTot[i]
+		gref[i]<-a1[stand[i]]+a2[stand[i]]*airTcent[i]+a3[stand[i]]*pastpr[Days[i],stand[i]]
+		S[i]<-b1[stand[i]]+b2[stand[i]]*airTcent[stand[i]]+b3[stand[i]]*pastpr[Days[i],stand[i]]
 		#Log transform light function slope to avoid numerical traps
 		#and allow for better mixing and faster convergence of the non-linear model
-		slope.temp[i]<-d1[stand[i]]+d2[stand[i]]*ATemp[i]+d3[stand[i]]*PrecipTot[i]
+		slope.temp[i]<-d1[stand[i]]+d2[stand[i]]*airTcent[i]+d3[stand[i]]*pastpr[Days[i],stand[i]]
 		l.slope[i]<-exp(slope.temp[i])
-	}
 	#conduct covariate centering to help with mixing
-	pastprcent[i]<-pastpr[i]-1
-	airTcent[i]<-airT[i]-airTmean
-	
-	
-	#calculate means for covariate centering
-	airTmean<-mean(airT[])
+
+	airTcent[i]<-airT[i]-airTmean	
+	}
+
 
 	#Antecedent calculations based on Ogle et al 2015
 	#calculate antecedent values for soil temp and soil water content
-
-	for(m in 1:Nlag){
-	#weights for precip
-	deltapr[m]~dgamma(1,1)
-	wpr[m]<-deltapr[m]/sumpr
-	#calculate weighted precip for each day in the past
-		for(i in 1:Ndays){
-			pr.temp[i,m]<-wpr[m]*a.pr[i,m]
+	for(j in 1:Nstand){
+		for(m in 1:Nlag){
+			#weights for precip
+			deltapr[m,j]~dgamma(1,1)
+			wpr[m,j]<-deltapr[m,j]/sumpr[j]
+			#calculate weighted precip for each day in the past
+			for(i in 1:Ndays){
+				pr.temp[i,m,j]<-wpr[m,j]*a.pr[i,m]
 			}
+		}
+
 	}
-
-
 	#calculate sums of unweighted delta values
 
-	sumpr<-sum(deltapr[])
+	sumpr[1]<-sum(deltapr[,1])
+	sumpr[2]<-sum(deltapr[,2])
 
 	#final antecedent calculations for soil values
 	for(i in 1:Ndays){
-		pastpr[i]<-sum(pr.temp[i,])
-
+		pastpr[i,1]<-sum(pr.temp[i,,1])
+		pastpr[i,2]<-sum(pr.temp[i,,2])
 	}
 	
 	#priors
 	#define prior distributions for parameters
 	#All parameters are given non-informative dist
 	
-	for(i in 1:Nparms){
-		a[i]~dnorm(0,.0001)
-		b[i]~dnorm(0,.0001)
-		d[i]~dnorm(0,.0001)
-		d.trans[i]<-exp(d[i])
+
+	for(i in 1:Nstand){
+		a1[i]~dnorm(0,.0001)
+		b1[i]~dnorm(0,.0001)
+		d1[i]~dnorm(0,.0001)
+		d.trans1[i]<-exp(d1[i])
+		a2[i]~dnorm(0,.0001)
+		b2[i]~dnorm(0,.0001)
+		d2[i]~dnorm(0,.0001)
+		d.trans2[i]<-exp(d2[i])
+		a3[i]~dnorm(0,.0001)
+		b3[i]~dnorm(0,.0001)
+		d3[i]~dnorm(0,.0001)
+		d.trans3[i]<-exp(d3[i])
 	}
 	tau.gs<-pow(sig.gs,-2)
 	sig.gs~dunif(0,1000)
--- gc_model/gc_model_script.r
+++ gc_model/gc_model_script.r
@@ -23,6 +23,22 @@ source("c:\\Users\\hkropp\\Documents\\GitHub\\larch_density_ecohydro\\sapflux_pr
 #libraries loaded from source
 #plyr, lubridate,caTools
 
+
+# libraries for parallel processing
+library(snow)
+library(snowfall)
+library(mcmcplots)
+
+#################################################################
+####specify directories                                   #######
+#################################################################
+#model output
+saveMdir <- c("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run1")
+#model code
+modCode <- "c:\\Users\\hkropp\\Documents\\GitHub\\larch_density_ecohydro\\gc_model\\gc_model_code.r"
+
+
+
 #################################################################
 ####read in datafiles                                     #######
 #################################################################
@@ -111,3 +127,201 @@ m.gcH17 <- datgcH17[datgcH17$D>=.6&datgcH17$PAR>5&datgcH17$Pr.mm<=1,]
 #################################################################
 ####organize data for model                               #######
 #################################################################
+#stand ID 1=low density, 2=high density
+#organize the data by sensor and stand
+#and omit missing sensor measurements
+tgcL <- list()
+for(i in 1:dim(datTreeL)[1]){
+	tgcL[[i]] <- na.omit(data.frame(doy=m.gcL$doy, 
+					year=m.gcL$year, hour=m.gcL$hour,
+					g.c=m.gcL[,i+3], D=m.gcL$D,PAR=m.gcL$PAR,treeID=rep(datTreeL$Tree[i], dim(m.gcL)[1]),
+					stand=rep(1, dim(m.gcL)[1])))
+}
+
+tgcL17 <- list()
+for(i in 1:dim(datTreeL17)[1]){
+	tgcL17[[i]] <- na.omit(data.frame(doy=m.gcL17$doy, 
+					year=m.gcL17$year, hour=m.gcL17$hour,
+					g.c=m.gcL17[,i+3], D=m.gcL17$D,PAR=m.gcL17$PAR,treeID=rep(datTreeL17$Tree[i], dim(m.gcL17)[1]),
+					stand=rep(1, dim(m.gcL17)[1])))
+}
+
+tgcH <- list()
+for(i in 1:dim(datTreeH)[1]){
+	tgcH[[i]] <- na.omit(data.frame(doy=m.gcH$doy, 
+					year=m.gcH$year, hour=m.gcH$hour,
+					g.c=m.gcH[,i+3], D=m.gcH$D,PAR=m.gcH$PAR,treeID=rep(datTreeH$Tree[i], dim(m.gcH)[1]),
+					stand=rep(2, dim(m.gcH)[1])))
+}
+tgcH17 <- list()
+for(i in 1:dim(datTreeH17)[1]){
+	tgcH17[[i]] <- na.omit(data.frame(doy=m.gcH17$doy, 
+					year=m.gcH17$year, hour=m.gcH17$hour,
+					g.c=m.gcH17[,i+3], D=m.gcH17$D,PAR=m.gcH17$PAR,treeID=rep(datTreeH17$Tree[i], dim(m.gcH17)[1]),
+					stand=rep(2, dim(m.gcH17)[1])))
+}
+
+
+#now turn into single data frame
+lowGC <-ldply(tgcL, data.frame)
+lowGC17 <-ldply(tgcL17, data.frame)
+highGC <-ldply(tgcH, data.frame)
+highGC17 <-ldply(tgcH17, data.frame)
+
+gcALL <- rbind(lowGC,lowGC17,highGC,highGC17)
+
+#first check how many tree X day X stand observations there are
+nCheck1 <- aggregate(gcALL$g.c, by=list(gcALL$doy,gcALL$year, gcALL$stand, gcALL$treeID), FUN="length")
+#check that there are at least multiple trees present in each day
+nCheck2 <- aggregate(nCheck1$x, by=list(nCheck1$Group.1,nCheck1$Group.2,nCheck1$Group.3), FUN="length")
+#check that there are how many measurements that are in each day
+nCheck3 <- aggregate(gcALL$g.c, by=list(gcALL$doy,gcALL$year, gcALL$stand), FUN="length")
+#this check is a better filter, because as long at there are at least 15 measurements the day is reasonable
+colnames(nCheck3) <- c("doy", "year", "stand", "count") 
+
+nCheck3 <- nCheck3[nCheck3$count>=15,]
+#now filter days that don't have at least 15 observations out
+nfilter <- nCheck3[,1:3]
+
+gcALLf <- join(gcALL, nfilter, by=c("doy", "year", "stand"), type="inner")
+
+#get unique combinations to generate IDS
+#standDayIDS
+standDay <- unique(data.frame(doy=gcALLf$doy,year=gcALLf$year,stand=gcALLf$stand))
+standDay$standDay <-seq(1, dim(standDay)[1])
+
+#join back into gcAll
+gcALL2 <- join(gcALLf, standDay, by=c("doy", "year", "stand"), type="left")
+
+#get average daily temperature for a covariate
+TairH <- aggregate(datHmet$Temp, by=list(datHmet$doy,datHmet$year), FUN="mean")
+colnames(TairH) <- c("doy","year","Tair")
+TairH$stand <- rep(2, dim(TairH)[1])
+
+TairL <- aggregate(datLmet$Temp, by=list(datLmet$doy,datLmet$year), FUN="mean")
+colnames(TairL) <- c("doy","year","Tair")
+TairL$stand <- rep(1, dim(TairL)[1])
+
+Tair <- rbind(TairH, TairL)
+
+#add air temp into the stand day da
+standDay2 <- join(standDay,Tair, by=c("doy","year", "stand"), type="left")
+
+
+#subset to get unique days
+Days <- unique(data.frame(doy=standDay2$doy,year=standDay2$year))
+Days <- Days[order(Days$year,Days$doy ),]
+Days$Days <- seq(1, dim(Days)[1])
+
+#add the Days ID back into standDay
+
+standDay2 <- join(standDay2, Days, by=c("doy", "year"), type="left")
+
+
+#now make a precip matrix that includes days into the past
+precipmat <- matrix(rep(NA, dim(Days)[1]*21), ncol=21)
+
+for(i in 1:dim(Days)[1]){
+	for(j in 1:21){
+		precipmat[i,j] <- datAirP$Pr.mm[datAirP$doy==(Days$doy[i]-j)&datAirP$year==Days$year[i]]
+	
+	}
+}
+#set up lag periods
+lagStart <- c(1,2,4,6,8,15)
+lagEnd <- c(1,3,5,7,14,21)
+
+#take averages over lag periods
+
+precipL <- matrix(rep(NA, dim(Days)[1]*length(lagStart)), ncol=length(lagStart))
+for(i in 1:dim(Days)[1]){
+	for(j in 1:length(lagStart)){
+		precipL[i,j] <- sum(precipmat[i,lagStart[j]:lagEnd[j]])
+	}	
+
+}
+
+#means for centering
+airTmean<- mean(standDay2$Tair)
+
+
+#################################################################
+####model run                                             #######
+#################################################################
+# run 3 parallel chains in R2OpenBugs because nonlinear and
+# antecedent model will likely not run well with the sampler in JAGS
+
+#1. specify data list
+
+datalist <- list(Nobs=dim(gcALL2)[1], gs=gcALL2$g.c, standDay=gcALL2$standDay, PAR=gcALL2$PAR,
+					D=gcALL2$D, NstandDay=dim(standDay2)[1], stand=standDay2$stand, airT=standDay2$Tair,
+					airTmean=airTmean,a.pr=precipL,Days=standDay2$Days,Nstand=2, Nlag=length(lagStart),
+					Ndays=dim(Days)[1])
+
+					
+# 2. setting the number of CPUs to be 3
+sfInit(parallel=TRUE, cpus=3)
+ 
+# 3. and assigning the R2OpenBUGS library to each CPU
+sfLibrary(R2OpenBUGS)					
+
+# 4. creating separate directory for each CPU process
+folder1 <- paste0(saveMdir, "\\chain1")
+folder2 <- paste0(saveMdir, "\\chain2")
+folder3 <- paste0(saveMdir, "\\chain3")
+dir.create(folder1); dir.create(folder2); dir.create(folder3)	
+folderALL <- c(folder1, folder2, folder3)
+
+for (i in 1:length(folderALL)){
+
+	file.copy(modCode, paste0(folderALL[i], "\\model_code.txt"), overwrite=TRUE) 
+}	
+					
+# 5. defining the function that will run MCMC on each CPU
+# Arguments:
+# chain - will be 1, 2 or 3
+# x.data - the data list
+# params - parameters to be monitored
+#folder - folder to save output in
+parallel.bugs <- function(chain,folder, x.data, params){
+
+ 	# 5a. specifying the initial MCMC values		
+	 inits <- function(){				
+		list(deltapr=matrix(rgamma(6*2,1,rate=1), ncol=2),
+				a1=rnorm(2,70,10), b1=rnorm(2,1.6,.1), d1=rnorm(2,-5,1),
+				a2=rnorm(2, -5,2), b2=rnorm(2, .1,.1), d2=rnorm(2,.5,.5),
+				a3=rnorm(2, 3,1), b3=rnorm(2,-.1,.1), d3=rnorm(2,.5,.5),sig.gs=rnorm(2,30,10))
+		}		
+	# 5b. call openbugs
+	bugs(data=x.data, inits=inits, parameters.to.save=params,
+             n.iter=3000, n.chains=1, n.burnin=1000, n.thin=1,
+             model.file="model_code.txt", codaPkg=TRUE,
+             OpenBUGS.pgm="C:/Program Files (x86)/OpenBUGS/OpenBUGS323/OpenBUGS.exe",debug=TRUE,
+             working.directory=folder)	
+}			 
+
+#6. set parameters to monitor
+parms <-c("wpr", "a1", "a2", "a3", "b1", "b2", "b3", "d1", "d2", "d3", "gref", "S", "l.slope","sig.gs")
+
+# 7. calling the sfLapply function that will run
+# parallel.bugs on each of the 3 CPUs
+sfLapply(1:3, fun=parallel.bugs, folder=folderALL,x.data=datalist, params=parms)
+
+
+
+# 8. locating position of each CODA chain
+chain1 <- paste(folder1, "/CODAchain1.txt", sep="")
+chain2 <- paste(folder2, "/CODAchain1.txt", sep="")
+chain3 <- paste(folder3, "/CODAchain1.txt", sep="")
+
+
+# 9. pull coda back out
+codaobj <- read.bugs(c(chain1, chain2, chain3))
+
+mcmcplots(codaobj, dir=saveMdir)
+
+modSum <-summary(codaobj) 
+
+
+write.table(modSum$statistics, paste0(saveMdir, "\\mod_stats.csv", sep=",", row.names=TRUE)
+write.table(modSum$quantiles, paste0(saveMdir, "\\mod_quants.csv", sep=",", row.names=TRUE)
