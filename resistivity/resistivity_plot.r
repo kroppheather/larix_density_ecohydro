c9056d1efc21134f85ea89c4934af500c8bfb025
kroppheather <kroppheather@gmail.com>
2017-10-13 13:19:06 -0400

    finished plots

--- resistivity/resistivity_plot.r
+++ resistivity/resistivity_plot.r
@@ -6,8 +6,21 @@
 ###########################################################################
 library(interp)
 library(plyr)
-#read in resistivity file
-rDB1 <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\resistivity\\DB1_model.csv")
+
+#################################################################
+####read in data                                          #######
+#################################################################
+
+#read in info about resistivity files to process
+datRQ <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\resistivity\\resistivity_quality.csv")
+
+
+#read in resistivity files
+datR <- list()
+for(i in 1:dim(datRQ)[1]){
+	datR[[i]] <- read.csv(paste0("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\resistivity\\file_out\\",datRQ$filename[i],".csv"))
+}
+
 
 #read in soil moisture
 datVWC <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\individual_data\\vwc_resitivity.csv")
@@ -18,135 +31,229 @@ datTD <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\individual_d
 
 #directory to save plots in
 
-plotDI <- "c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\resistivity_plot"
+plotDI <- "c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\resistivity_plot\\out_R"
 
-#subset
-vDB1 <-datVWC[datVWC$file.name=="DB1",]
-#calculate correct dist in m (start at 20 because broken tape)
-vDB1$x <- (vDB1$transect.distance-20)/100
 
-tdDB1 <- datTD[datTD$file_label=="DB1",]
-tdDB1$x <- (tdDB1$distance.on.transect-20)/100
+#################################################################
+####deal with any electordes measured from other side     #######
+####of the start of the transect tape                     #######
+#################################################################
+to.fix <- which(datRQ$reversed.start=="y")
 
-#get range of resistivity
-range(rDB1$Resistivity)
+for(i in 1:length(to.fix)){
+	datR[[to.fix[i]]]$X <- datRQ$transect.start[to.fix[i]]-datR[[to.fix[i]]]$X 
+
+}
+
+#################################################################
+####offset paired data on transects that didn't           #######
+#### start at zero                                        #######
+#################################################################
+to.offs <- data.frame(filename=as.character(datRQ$filename[which(datRQ$transect.start>0&datRQ$reversed.start=="n")]))
+to.offs$offD <-datRQ$transect.start[which(datRQ$transect.start>0&datRQ$reversed.start=="n")]/100
+#all distances are in cm in both files so divide all by 100
+datVWC$x <- datVWC$transect.distance/100
+datTD$x <- datTD$distance.on.transect/100
+datVWC$file.name <- as.character(datVWC$file.name)
+datTD$file_label <- as.character(datTD$file_label)
+
+#now offest distances that don't start at zero
+for(i in 1:dim(to.offs)[1]){
+	datVWC$x[datVWC$file.name==to.offs$filename[i]] <- datVWC$x[datVWC$file.name==to.offs$filename[i]]-to.offs$offD[i]
+	datTD$x[datTD$file_label==to.offs$filename[i]] <- datTD$x[datTD$file_label==to.offs$filename[i]]-to.offs$offD[i]
+}
+
+#################################################################
+####interpolate for plots                                 #######
+#################################################################
+
 #set limits for plotting
 #get range for plots
-yh <-0
-yl <-min(rDB1$Depth)
-xl <- 0
-xh <- max(rDB1$X)
 
-xo<-seq(xl,xh, length.out=600)
-yo<-seq(yl,yh, length.out=600)
+yh <- numeric(0)
+yl <- numeric(0)
+xh <- numeric(0)
+xl <- numeric(0)
+rspInt <- list()
+res.sp <- list()
+
+for(i in 1:dim(datRQ)[1]){
+	yh[i] <-0
+	yl[i] <-min(datR[[i]]$Depth)
+	xl[i] <- 0
+	xh[i] <- max(datR[[i]]$X)
+
+	rspInt <- interp(x=datR[[i]]$X, y=datR[[i]]$Depth, z=datR[[i]]$Resistivity, xo=seq(xl[i],xh[i], length.out=600),
+					yo=seq(yl[i],yh[i], length.out=600), extrap=FALSE)
+
+	#deconstruct into a vector
+	res.sp[[i]]  <- data.frame(x=rep(rspInt$x, times=600), y=rep(rspInt$y, each=600), z=as.vector(rspInt$z))	
 
 
-
-DB1i <- interp(x=rDB1$X, y=rDB1$Depth, z=rDB1$Resistivity, xo=seq(xl,xh, length.out=600),
-				yo=seq(yl,yh, length.out=600), extrap=FALSE)
-
-#deconstruct into a vector
-DB1sp <- data.frame(x=rep(DB1i$x, times=600), y=rep(DB1i$y, each=600), z=as.vector(DB1i$z))	
-
-DBrange <-range(na.omit(DB1sp$z))
-	
-#create color
+}	
+#################################################################
+####create color for plots                                #######
+#################################################################
 
 a <-colorRampPalette(c("royalblue4","lightcyan2","forestgreen", "darkorange3", "firebrick3"))(11)
-#make breaks 
-DBrange <-range(na.omit(DB1sp$z))
-#pull the upper value because want breaks above
-DBrange[1] <- round_any(DBrange[1],10, ceiling)
-DBrange[2] <- round_any(DBrange[2],10, ceiling)
-lrdb <- log(DBrange)
-logbreak<-seq(lrdb[1],lrdb[2] , length.out=11)
-rdbreak<- exp(logbreak)
+
+#################################################################
+####match resistivity to plot color                       #######
+#################################################################
+DBrange <- list()
+lrdb <- list()
+logbreak <-list()
+rdbreak <- list()
+for(i in 1:dim(datRQ)[1]){
+	#make breaks 
+	DBrange[[i]] <-range(na.omit(res.sp[[i]]$z))
+	#pull the upper value because want breaks above
+	DBrange[[i]][1] <- round_any(DBrange[[i]][1],10, ceiling)
+	DBrange[[i]][2] <- round_any(DBrange[[i]][2],10, ceiling)
+	#take the log of these range values
+	lrdb[[i]] <- log(DBrange[[i]])
+	logbreak[[i]]<-seq(lrdb[[i]][1],lrdb[[i]][2] , length.out=11)
+	rdbreak[[i]]<- exp(logbreak[[i]])
 	
 
-DB1sp$col <- ifelse(is.na(DB1sp$z), "white",
-			ifelse(DB1sp$z<=rdbreak[1],a[1],
-			ifelse(DB1sp$z>rdbreak[1]&DB1sp$z<=rdbreak[2],a[2],
-			ifelse(DB1sp$z>rdbreak[2]&DB1sp$z<=rdbreak[3],a[3],
-			ifelse(DB1sp$z>rdbreak[3]&DB1sp$z<=rdbreak[4],a[4],
-			ifelse(DB1sp$z>rdbreak[4]&DB1sp$z<=rdbreak[5],a[5],
-			ifelse(DB1sp$z>rdbreak[5]&DB1sp$z<=rdbreak[6],a[6],
-			ifelse(DB1sp$z>rdbreak[6]&DB1sp$z<=rdbreak[7],a[7],
-			ifelse(DB1sp$z>rdbreak[7]&DB1sp$z<=rdbreak[8],a[8],
-			ifelse(DB1sp$z>rdbreak[8]&DB1sp$z<=rdbreak[9],a[9],
-			ifelse(DB1sp$z>rdbreak[9]&DB1sp$z<=rdbreak[10],a[10],
-			ifelse(DB1sp$z>rdbreak[10]&DB1sp$z<=rdbreak[11],a[11],"black"
+res.sp[[i]]$col <- ifelse(is.na(res.sp[[i]]$z), "white",
+			ifelse(res.sp[[i]]$z<=rdbreak[[i]][1],a[1],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][1]&res.sp[[i]]$z<=rdbreak[[i]][2],a[2],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][2]&res.sp[[i]]$z<=rdbreak[[i]][3],a[3],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][3]&res.sp[[i]]$z<=rdbreak[[i]][4],a[4],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][4]&res.sp[[i]]$z<=rdbreak[[i]][5],a[5],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][5]&res.sp[[i]]$z<=rdbreak[[i]][6],a[6],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][6]&res.sp[[i]]$z<=rdbreak[[i]][7],a[7],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][7]&res.sp[[i]]$z<=rdbreak[[i]][8],a[8],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][8]&res.sp[[i]]$z<=rdbreak[[i]][9],a[9],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][9]&res.sp[[i]]$z<=rdbreak[[i]][10],a[10],
+			ifelse(res.sp[[i]]$z>rdbreak[[i]][10]&res.sp[[i]]$z<=rdbreak[[i]][11],a[11],"black"
 			))))))))))))
+}
+#################################################################
+####potentially unused                                    #######
+#################################################################
 
-fig.yup<- 1/11
-fig.lab<- seq(1,11)*fig.yup
 #now make an interpolation for the figure
 
-break.fig <- data.frame(x=rep(c(0,.5,1), each=11), y=rep(seq(0,1, length=11), times=3), z=rep(lab.break, times=3)	)
+#break.fig <- data.frame(x=rep(c(0,.5,1), each=11), y=rep(seq(0,1, length=11), times=3), z=rep(lab.break, times=3)	)
 
+
+
+#################################################################
+####subset vwc and match soil vwc to plot color           #######
+#################################################################
+#subset vwc
+vwcS <- list()
+for(i in 1:dim(datRQ)[1]){
+	vwcS[[i]] <- datVWC[datVWC$file.name==datRQ$filename[i],]
+}
 #create color range for soil moisture
-vrange <- range(vDB1$Mineral.VWC)
-#break up into 11
-vl <-0.01
-vu <- round_any(vrange[2],.1, ceiling)
-lvbreaks <- seq(log(vl), log(vu), length=11)
-vupper <- exp(lvbreaks)
+vrange <- list()
+lvbreaks <- list()
+vupper <-list()
+for(i in 1:dim(datRQ)[1]){
+
+	vrange[[i]] <- range(na.omit(vwcS[[i]]$Mineral.VWC))
+	#break up into 11
+	vrange[[i]][1] <-0.01
+	vrange[[i]][2] <- round_any(vrange[[i]][2],.1, ceiling)
+	lvbreaks[[i]] <- seq(log(vrange[[i]][1]), log(vrange[[i]][2]), length=11)
+	vupper[[i]] <- exp(lvbreaks[[i]])
 
 
-vDB1$col <- ifelse(vDB1$Mineral.VWC<=vupper[1],a[11],
-			ifelse(vDB1$Mineral.VWC>vupper[1]&vDB1$Mineral.VWC<=vupper[2],a[10],
-			ifelse(vDB1$Mineral.VWC>vupper[2]&vDB1$Mineral.VWC<=vupper[3],a[9],
-			ifelse(vDB1$Mineral.VWC>vupper[3]&vDB1$Mineral.VWC<=vupper[4],a[8],
-			ifelse(vDB1$Mineral.VWC>vupper[4]&vDB1$Mineral.VWC<=vupper[5],a[7],
-			ifelse(vDB1$Mineral.VWC>vupper[5]&vDB1$Mineral.VWC<=vupper[6],a[6],
-			ifelse(vDB1$Mineral.VWC>vupper[6]&vDB1$Mineral.VWC<=vupper[7],a[5],
-			ifelse(vDB1$Mineral.VWC>vupper[7]&vDB1$Mineral.VWC<=vupper[8],a[4],
-			ifelse(vDB1$Mineral.VWC>vupper[8]&vDB1$Mineral.VWC<=vupper[9],a[3],
-			ifelse(vDB1$Mineral.VWC>vupper[9]&vDB1$Mineral.VWC<=vupper[10],a[2],
-			ifelse(vDB1$Mineral.VWC>vupper[10]&vDB1$Mineral.VWC<=vupper[11],a[1],"black")))))))))))
-
-			
-jpeg(paste0(plotDI, "\\DB1_out.jpg") , width=1500, height=700, units="px", quality=100)			
-ab <- layout(matrix(seq(1,3), ncol=3), width=c(lcm(36), lcm(4), lcm(4)), height=c(lcm(15), lcm(15), lcm(15)))
-layout.show(ab)
-par(mai=c(0,0,0,0))		
-plot(c(0,1), c(0,1), type="n", ylim=c(-.62,yh), xlim=c(xl,xh), ylab=" ", xlab=" ", axes=FALSE, yaxs="i", xaxs="i")		
-points(DB1sp$x,DB1sp$y,pch=19, col=DB1sp$col)
-
-points(tdDB1$x, -tdDB1$thaw.depth/100, type="l", lwd=5, col="thistle4")
-axis(2, seq(-.65,0, by=.05), las=2, cex.axis=2.5)
-mtext("Depth (m)", side=2, line=8, cex=3)
-axis(1, seq(0.1,2.5, by=.1), cex.axis=2.5)
-mtext("Transect distance (m)", side=1, line=5, cex=3)
-
-for(i in 1:dim(vDB1)[1]){
-	polygon(c(vDB1$x[i],vDB1$x[i],vDB1$x[i]+.1,vDB1$x[i]+.1), c(-.015,0,0,-.015),
-			col=vDB1$col[i],border=FALSE)
-
-}
-legend(1,-.45, c("thaw depth"), lwd=5, col="thistle4", bty="n", cex=3)
-
-par(mai=c(0,.5,0,.5))
-plot(c(0,1), c(0,1), type="n", ylim=c(0,1), xlim=c(0,1), ylab=" ", xlab=" ", axes=FALSE, yaxs="i", xaxs="i")
-
-for(i in 1:length(a)){
-	polygon(c(0,0,1,1), c(fig.yup*(i-1),fig.yup*i,fig.yup*i,fig.yup*(i-1)), col=a[i], border=FALSE)
-
+vwcS[[i]]$col <- ifelse(vwcS[[i]]$Mineral.VWC<=vupper[[i]][1],a[11],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][1]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][2],a[10],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][2]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][3],a[9],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][3]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][4],a[8],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][4]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][5],a[7],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][5]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][6],a[6],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][6]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][7],a[5],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][7]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][8],a[4],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][8]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][9],a[3],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][9]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][10],a[2],
+			ifelse(vwcS[[i]]$Mineral.VWC>vupper[[i]][10]&vwcS[[i]]$Mineral.VWC<=vupper[[i]][11],a[1],"black")))))))))))
 }
 
-axis(4, c(0,fig.lab), c(0,round(rdbreak,0)), las=2, cex.axis=2.5 )
-mtext("Resistivity ", side=4, line=8, cex=3)
 
-par(mai=c(0,1,0,0))
-plot(c(0,1), c(0,1), type="n", ylim=c(0,1), xlim=c(0,1), ylab=" ", xlab=" ", axes=FALSE, yaxs="i", xaxs="i")
-
-for(i in 1:length(a)){
-	polygon(c(0,0,1,1), c(fig.yup*(i-1),fig.yup*i,fig.yup*i,fig.yup*(i-1)), col=a[i], border=FALSE)
-
+#################################################################
+####subset thaw depth                                     #######
+#################################################################
+tdS <- list()
+for(i in 1:dim(datRQ)[1]){
+	tdS[[i]] <- datTD[datTD$file_label==datRQ$filename[[i]],]
 }
 
-axis(4, c(0,fig.lab), c(round(rev(vupper),3),0), las=2, cex.axis=2.5 )
-mtext("Volumetric soil moisture ", side=4, line=9, cex=3)
-dev.off()
+
+#################################################################
+####make resistivity plots                                #######
+#################################################################
+#get depth coordinates
+yl <- numeric(0)
+yh <- numeric(0)
+xl <- numeric(0)
+yh <- numeric(0)
+for(i in 1:dim(datRQ)[1]){
+	yl[i] <- max(tdS[[i]]$thaw.depth/100,abs(datR[[i]]$Depth))
+	yh[i] <- 0
+	xl[i] <- 0
+	xh[i] <- max(datR[[i]]$X)
+}
+yoff <- .05
+xoff <- .2
+fig.yup<- 1/11
+fig.lab<- seq(1,11)*fig.yup
+
+
+
+for(i in 1:dim(datRQ)[1]){			
+	jpeg(paste0(plotDI, "\\",datRQ$filename[i],".jpg") , width=1500, height=700, units="px", quality=100)			
+	ab <- layout(matrix(seq(1,3), ncol=3), width=c(lcm(36), lcm(4), lcm(4)), height=c(lcm(15), lcm(15), lcm(15)))
+	layout.show(ab)
+	par(mai=c(0,0,0,0))		
+	plot(c(0,1), c(0,1), type="n", ylim=c(-(yl[i]+yoff),yh[i]), xlim=c(xl[i],xh[i]), ylab=" ", xlab=" ", axes=FALSE, yaxs="i", xaxs="i")		
+	#plot resistivity
+	points(res.sp[[i]]$x,res.sp[[i]]$y,pch=19, col=res.sp[[i]]$col)
+	#plot thaw depth
+	points(tdS[[i]]$x, -tdS[[i]]$thaw.depth/100, type="l", lwd=5, col="thistle4")
+	#axes
+	axis(2, seq(-1,0, by=.05), las=2, cex.axis=2.5)
+	mtext("Depth (m)", side=2, line=8, cex=3)
+	axis(1, seq(0.1,2.5, by=.1), cex.axis=2.5)
+	mtext("Transect distance (m)", side=1, line=5, cex=3)
+	#soil moisture
+	for(j in 1:dim(vwcS[[i]])[1]){
+		polygon(c(vwcS[[i]]$x[j],vwcS[[i]]$x[j],vwcS[[i]]$x[j]+.1,vwcS[[i]]$x[j]+.1), c(-.01,0,0,-.01),
+				col=vwcS[[i]]$col[j],border=FALSE)
+
+	}
+	#add legend
+	legend(1,-.45, c("thaw depth"), lwd=5, col="thistle4", bty="n", cex=3)
+
+	par(mai=c(0,.5,0,.5))
+	plot(c(0,1), c(0,1), type="n", ylim=c(0,1), xlim=c(0,1), ylab=" ", xlab=" ", axes=FALSE, yaxs="i", xaxs="i")
+	#plot the restivity legend
+	for(j in 1:length(a)){
+		polygon(c(0,0,1,1), c(fig.yup*(j-1),fig.yup*j,fig.yup*j,fig.yup*(j-1)), col=a[j], border=FALSE)
+
+	}
+	#axis labels
+	axis(4, c(0,fig.lab), lab=c(0,round(rdbreak[[i]],0)), las=2, cex.axis=2.5 )
+	mtext("Resistivity ", side=4, line=8, cex=3)
+	#plot the vwc legend
+	par(mai=c(0,1,0,0))
+	plot(c(0,1), c(0,1), type="n", ylim=c(0,1), xlim=c(0,1), ylab=" ", xlab=" ", axes=FALSE, yaxs="i", xaxs="i")
+
+	for(j in 1:length(a)){
+		polygon(c(0,0,1,1), c(fig.yup*(j-1),fig.yup*j,fig.yup*j,fig.yup*(j-1)), col=a[j], border=FALSE)
+
+	}
+
+	axis(4, c(0,fig.lab), c(round(rev(vupper[[i]]),3),0), las=2, cex.axis=2.5 )
+	mtext("Volumetric soil moisture ", side=4, line=9, cex=3)
+	dev.off()
+
+}
 
 
 #now compare modelled resitivity in the 0.025 and the soil moisture
