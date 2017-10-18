51982d259629e2749ce68943795057d90e99e496
kroppheather <kroppheather@gmail.com>
2017-10-18 15:48:51 -0400

    finished initial look at soil profile

--- gc_model/gc_model_script.r
+++ gc_model/gc_model_script.r
@@ -326,15 +326,15 @@ folder3 <- paste0(saveMdir, "\\out\\chain3\\")
 # 9. pull coda back out
 codaobj1 <- read.bugs(c(paste0(folder1, "\\CODAchain1a.txt"),
 						paste0(folder2, "\\CODAchain1a.txt")
-						#,paste0(folder3, "\\CODAchain1a.txt")
+						,paste0(folder3, "\\CODAchain1a.txt")
 						))
 
 
 plot(codaobj1, ask=TRUE)
-mcmcplot(codaobj1, dir=saveMdir)
+mcmcplot(codaobj1, dir=paste0(saveMdir, "\\out"))
 
 modSum <-summary(codaobj1) 
 
 
-write.table(modSum$statistics, paste0(saveMdir, "\\mod_stats.csv"), sep=",", row.names=TRUE)
-write.table(modSum$quantiles, paste0(saveMdir, "\\mod_quants.csv"), sep=",", row.names=TRUE)
+write.table(modSum$statistics, paste0(saveMdir, "\\out", "\\mod_stats.csv"), sep=",", row.names=TRUE)
+write.table(modSum$quantiles, paste0(saveMdir, "\\out", "\\mod_quants.csv"), sep=",", row.names=TRUE)
--- soil/soil_profile.r
+++ soil/soil_profile.r
@@ -134,34 +134,125 @@ colnames(datDayO) <- c("doy","mossI", "stand", "vwc")
 #################################################################
 ####plot aggregate profile data                           #######
 #################################################################
+xl2016 <- 185
+xh2016 <-245
+xl2017 <- 165
+xh2017 <- 195
+subAirP <- datAirP[datAirP$doy>=xl2017&datAirP$year==2017&datAirP$doy<=xh2017 ,]
+subAirP2 <- datAirP[datAirP$doy>=xl2016&datAirP$year==2016&datAirP$doy<=xh2016 ,]
+wd <- 30
+hd <- 25
 
-jpeg(paste0(pdir, "summ_profile.jpg"), , width=1000, height=1000, units="px")
-par(mfrow=c(2,1))
-plot(c(0,1),c(0,1), xlim=c(165,195), ylim=c(0,1), xlab="Day of Year", ylab ="swc (cm3/cm3) and Precip(cm) ", xaxs="i", yaxs="i", 
-		type="n")
+PRmax <- 50
+Prscale <- 1/PRmax
+
+colS <- "royalblue"
+colT <- "darkgreen"
+colHt <- rgb(205/255,79/255,57/255, .5)
+colLt <- rgb(65/255,105/255,225/255,.5)
+
+colM <-"palegreen4"
+colOM <- "tomato2"
+colO <- "saddlebrown"
+axisC <-3
+
+jpeg(paste0(pdir, "summ_profile_all.jpg"), , width=2200, height=2200, units="px")
+ab <- layout(matrix(seq(1,4), ncol=2, byrow=FALSE), width=rep(lcm(wd),4), height=rep(lcm(hd),4))
+
+par(mai=c(0,0,0,0))
+plot(c(0,1),c(0,1), xlim=c(xl2016,xh2016), ylim=c(0,1), xlab=" ", ylab ="  ", xaxs="i", yaxs="i", 
+		type="n", axes=FALSE)
+	
+	for(i in 1:dim(subAirP2)[1]){
+		polygon(c(subAirP2$doy[i]-1,subAirP2$doy[i]-1,subAirP2$doy[i],subAirP2$doy[i]), 
+			c(0,subAirP2$Pr.mm[i]*Prscale,subAirP2$Pr.mm[i]*Prscale,0), col="grey60", border=FALSE)
+
+}	
+	
+	
+	points(datSW$doy[datSW$site=="ld"&datSW$sensorLoc=="tree"&datSW$sensorZ==5&datSW$year==2016&datSW$doy>195]+
+		(datSW$hour[datSW$site=="ld"&datSW$sensorLoc=="tree"&datSW$sensorZ==5&datSW$year==2016&datSW$doy>195]/24),
+		datSW$vwc.GS3[datSW$site=="ld"&datSW$sensorLoc=="tree"&datSW$sensorZ==5&datSW$year==2016&datSW$doy>195],
+		type="l",  col=colT, lwd=6)
+	points(datSW$doy[datSW$site=="ld"&datSW$sensorLoc=="shrub"&datSW$sensorZ==5&datSW$year==2016&datSW$doy>195]+
+		(datSW$hour[datSW$site=="ld"&datSW$sensorLoc=="shrub"&datSW$sensorZ==5&datSW$year==2016&datSW$doy>195]/24),
+		datSW$vwc.GS3[datSW$site=="ld"&datSW$sensorLoc=="shrub"&datSW$sensorZ==5&datSW$year==2016&datSW$doy>195],
+		type="l", lty=1, col=colS, lwd=6)	
+	legend(xl2016+5, 1, c("5 cm organic under tree","5 cm organic under shrub","Precipitation"),
+		col=c(colT,colS,"grey60"), lwd=c(6,6,NA), pch=c(NA,NA,15),
+			lty=c(1,1,NA), bty="n", cex=4)
+			
+	axis(2, seq(0,1, by=.2), las=2,  cex.axis=axisC, lwd.ticks=3)
+	mtext("Volumetric soil water (m3/m3)", side=2, outer=TRUE, line=-9, cex=4)	
+	mtext("Precipitation (mm)", side=4, outer=TRUE, line=-9, cex=4)	
+box(which="plot")
+
+
+par(mai=c(0,0,0,0))
+plot(c(0,1),c(0,1), xlim=c(xl2016,xh2016), ylim=c(0,1), xlab=" ", ylab ="  ", xaxs="i", yaxs="i", 
+		type="n", axes=FALSE)
+	#points(datSW$doy[datSW$site=="hd"&datSW$sensorLoc=="moss"&datSW$sensorZ==5&datSW$year==2016]+
+	#	(datSW$hour[datSW$site=="hd"&datSW$sensorLoc=="moss"&datSW$sensorZ==5&datSW$year==2016]/24),
+	#	datSW$vwc.GS3[datSW$site=="hd"&datSW$sensorLoc=="moss"&datSW$sensorZ==5&datSW$year==2016],
+	#	type="l",  col=colT, lwd=6)
+		for(i in 1:dim(subAirP2)[1]){
+		polygon(c(subAirP2$doy[i]-1,subAirP2$doy[i]-1,subAirP2$doy[i],subAirP2$doy[i]), 
+			c(0,subAirP2$Pr.mm[i]*Prscale,subAirP2$Pr.mm[i]*Prscale,0), col="grey60", border=FALSE)
+
+}		
+	points(datSW$doy[datSW$site=="hd"&datSW$sensorLoc=="organic"&datSW$sensorZ==5&datSW$year==2016]+
+		(datSW$hour[datSW$site=="hd"&datSW$sensorLoc=="organic"&datSW$sensorZ==5&datSW$year==2016]/24),
+		datSW$vwc.GS3[datSW$site=="hd"&datSW$sensorLoc=="organic"&datSW$sensorZ==5&datSW$year==2016],
+		type="l", lty=1, col=colT, lwd=6)			
+	axis(1,seq(xl2016,xh2016-5,by=5) ,rep(" ", length(seq(xl2016,xh2016-5,by=5))),   cex.axis=axisC, lwd.ticks=3)	
+	mtext(seq(xl2016,xh2016-5, by=5),at=seq(xl2016,xh2016-5, by=5), line=3, side=1, cex=2.5)
+	axis(2, seq(0,.8, by=.2), las=2,  cex.axis=axisC, lwd.ticks=3)	
+legend(xl2016+5, 1, c("5 cm organic under tree", "Precipitation"), col=c(colT, "grey60"), lwd=c(6,NA),
+			lty=c(1,NA), pch=c(NA,15), bty="n", cex=4)	
+	
+box(which="plot")
+
+par(mai=c(0,0,0,0))
+plot(c(0,1),c(0,1), xlim=c(xl2017,xh2017), ylim=c(0,1), xlab=" ", ylab ="  ", xaxs="i", yaxs="i", 
+		type="n", axes=FALSE)
 for(i in 1:dim(subAirP)[1]){
 	polygon(c(subAirP$doy[i]-1,subAirP$doy[i]-1,subAirP$doy[i],subAirP$doy[i]), 
-			c(0,subAirP$Pr.mm[i]/10,subAirP$Pr.mm[i]/10,0), col="grey60", border=FALSE)
+			c(0,subAirP$Pr.mm[i]*Prscale,subAirP$Pr.mm[i]*Prscale,0), col="grey60", border=FALSE)
 
 }		
 		
 points(datDayO$doy[datDayO$stand=="ld"&datDayO$mossI==1],datDayO$vwc[datDayO$stand=="ld"&datDayO$mossI==1],
-			col="tomato3",pch=19, type="b")	
+			col=colOM,pch=19, type="b", cex=4, lwd=3)	
 points(datDayO$doy[datDayO$stand=="ld"&datDayO$mossI==2],datDayO$vwc[datDayO$stand=="ld"&datDayO$mossI==2],
-	col="saddlebrown",pch=19, type="b")
+	col="saddlebrown",pch=19, type="b", cex=4, lwd=3)
 points(datDayM$doy[datDayM$stand=="ld"],datDayM$vwc[datDayM$stand=="ld"],
-	col="palegreen4",pch=19, type="b")	
-	
-plot(c(0,1),c(0,1), xlim=c(165,195), ylim=c(0,1), xlab="Day of Year", ylab ="swc (cm3/cm3) and Precip(cm) ", xaxs="i", yaxs="i", 
-		type="n")
+	col=colM,pch=19, type="b", cex=4, lwd=3)	
+axis(4, seq(0,1, by=.2),seq(0,1, by=.2)*(1/Prscale) , las=2,  cex.axis=axisC, lwd.ticks=3)
+legend(xl2017+12, 1, c("organic below moss","moss","organic", "Precipitation"), col=c(colOM,colM,  colO, "grey60"), 
+			lwd=c(6,6,6,NA),
+			lty=c(1,1,1,NA), pch=c(NA,NA,NA,15), bty="n", cex=4)		
+box(which="plot")
+
+
+par(mai=c(0,0,0,0))	
+plot(c(0,1),c(0,1), xlim=c(xl2017,xh2017), ylim=c(0,1), xlab=" ", ylab ="  ", xaxs="i", yaxs="i", 
+		type="n", axes=FALSE)
 for(i in 1:dim(subAirP)[1]){
 	polygon(c(subAirP$doy[i]-1,subAirP$doy[i]-1,subAirP$doy[i],subAirP$doy[i]), 
-			c(0,subAirP$Pr.mm[i]/10,subAirP$Pr.mm[i]/10,0), col="grey60", border=FALSE)
+			c(0,subAirP$Pr.mm[i]*Prscale,subAirP$Pr.mm[i]*Prscale,0), col="grey60", border=FALSE)
 
 }		
 	
 points(datDayO$doy[datDayO$stand=="hd"],datDayO$vwc[datDayO$stand=="hd"],
-			col="saddlebrown",pch=19, type="b")	
+			col="saddlebrown",pch=19, type="b", cex=4, lwd=3)	
 points(datDayM$doy[datDayM$stand=="hd"],datDayM$vwc[datDayM$stand=="hd"],
-	col="palegreen4",pch=19, type="b")	
+	col="palegreen4",pch=19, type="b", cex=4, lwd=3)
+	axis(1,seq(xl2017,xh2017-5,by=5) ,rep(" ", length(seq(xl2017,xh2017-5,by=5))),   cex.axis=axisC, lwd.ticks=3)	
+	mtext(seq(xl2017,xh2017-5, by=5),at=seq(xl2017,xh2017-5, by=5), line=3, side=1, cex=2.5)
+legend(xl2017+2, 1, c("organic below moss","moss", "Precipitation"), col=c( colOM,colM, "grey60"), 
+			lwd=c(6,6,NA),
+			lty=c(1,1,NA), pch=c(NA,NA,15), bty="n", cex=4)			
+box(which="plot")	
+axis(4, seq(0,.8, by=.2),seq(0,.8, by=.2)*(1/Prscale) , las=2,  cex.axis=axisC, lwd.ticks=3)
+mtext("Day of year", side=1, outer=TRUE, line=-25, cex=4)
 dev.off()
