c88fbb7053eda968b941bfb0df2d0e63a55cceee
kroppheather <kroppheather@gmail.com>
2018-02-02 13:58:23 -0500

    started making figures

--- gc_model/gc_model_figure.r
+++ gc_model/gc_model_figure.r
@@ -10,18 +10,18 @@
 library(plyr)
 
 #set plot directory
-plotDI <- "c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\plots\\run39"
+plotDI <- "c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\plots\\run42"
 
 
 #read in stand day data
 
-daySD <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run39\\out\\standDay.csv")
-datgc <-read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run39\\out\\gcdata.csv")
+daySD <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run42\\out\\standDay.csv")
+datgc <-read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run42\\out\\gcdata.csv")
 #################################################################
 ####read in parameters                                    #######
 #################################################################
-datM <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run39\\out\\mod_stats.csv")
-datQ <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run39\\out\\mod_quants.csv")
+datM <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run42\\out\\mod_stats.csv")
+datQ <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\gc_model\\run42\\out\\mod_quants.csv")
 
 #stand index of 1 is low
 
@@ -56,8 +56,8 @@ datparm <- datC[datC$parms3=="a"|datC$parms3=="b"|datC$parms3=="d",]
 
 datgrefA <- join(datgrefA, datprecip, by=c("Days", "stand"),type="left")
 
-datparm$stand <- rep(c(1,2), times=12)
-datparm$pN <- rep(rep(c(1,2,3,4),each=2),times=3)
+datparm$stand <- rep(c(1,2), times=9)
+datparm$pN <- rep(rep(c(1,2,3),each=2),times=3)
 
 
 datw<- datC[datC$parms3=="wpr",]
@@ -71,6 +71,7 @@ sdAll <- join(daySD, datprecip, by=c("Days","stand"), type="left")
 #################################################################
 ####goodness of fit plots                                 #######
 #################################################################
+#make a plot of the fit
 plot(datgc$g.c,datrep$Mean, pch=19, xlim=c(0,80), ylim=c(0,80))
 fit<-lm(datrep$Mean~datgc$g.c)
 summary(fit)
@@ -95,7 +96,7 @@ Smax <-1.7
 Tmin <-5
 Tmax <- 25
 Pmin <-0
-Pmax <- 30
+Pmax <- 35
 colL <-"royalblue"
 colH <- "tomato3"
 axisC <-3
@@ -113,8 +114,8 @@ Pseq <- seq(Pmin,Pmax, by=.1)
 STseq <- seq(STmin,STmax, by=.1)
 
 
-jpeg(paste0(plotDI , "\\regression_coeff.jpg"), width=4000, height=2000, units="px", quality=100)
-	ab <- layout(matrix(seq(1,6), ncol=3, byrow=FALSE), width=rep(lcm(wd),6), height=rep(lcm(hd),6))
+jpeg(paste0(plotDI , "\\regression_coeff.jpg"), width=3000, height=2000, units="px", quality=100)
+	ab <- layout(matrix(seq(1,4), ncol=2, byrow=FALSE), width=rep(lcm(wd),4), height=rep(lcm(hd),4))
 	
 par(mai=c(0,0,0,0))
 	plot(c(0,1),c(0,1), type="n", xlim=c(Tmin,Tmax), ylim=c(gmin,gmax), axes=FALSE, xaxs="i", yaxs="i",
@@ -249,74 +250,9 @@ par(mai=c(0,0,0,0))
 	mtext("Past precipitation over 2 months (mm)",side=1, line=8, cex=4)
 box(which="plot")	
 
-
-
-par(mai=c(0,0,0,0))
-	plot(c(0,1),c(0,1), type="n", xlim=c(STmin,STmax), ylim=c(gmin,gmax), axes=FALSE, xaxs="i", yaxs="i",
-			xlab=" ", ylab=" ")
-	arrows(datgrefA$T.Dcm2, datgrefA$X2.5.,datgrefA$T.Dcm2, datgrefA$X97.5.,lwd=2,code=0 )
-	points(datgrefA$T.Dcm2[datgrefA$stand==1], datgrefA$Mean[datgrefA$stand==1],
-			col=colL, pch=19, cex=4)
-	points(datgrefA$T.Dcm2[datgrefA$stand==2], datgrefA$Mean[datgrefA$stand==2],
-			col=colH, pch=19, cex=4)
-
-			
-	if(datparm$Sig[datparm$parms3=="a"&datparm$stand==1&datparm$pN==4]==1){
-		points(STseq,regL(STseq,datparm$Mean[datparm$parms3=="a"&datparm$stand==1&datparm$pN==1],
-				datparm$Mean[datparm$parms3=="a"&datparm$stand==1&datparm$pN==4],5.04), type="l", lwd=4, col=colL)
-	}else{
-		abline(h=datparm$Mean[datparm$parms3=="a"&datparm$stand==1&datparm$pN==1], lwd=4, lty=3, col=colL)
-	}
-	
-	if(datparm$Sig[datparm$parms3=="a"&datparm$stand==2&datparm$pN==4]==1){
-		points(STseq,regL(STseq,datparm$Mean[datparm$parms3=="a"&datparm$stand==2&datparm$pN==1],
-				datparm$Mean[datparm$parms3=="a"&datparm$stand==2&datparm$pN==4],1.74), type="l", lwd=4, col=colH)
-	}else{
-		abline(h=datparm$Mean[datparm$parms3=="a"&datparm$stand==2&datparm$pN==1], lwd=4, lty=3, col=colH)
-	}	
-
-box(which="plot")
-
-par(mai=c(0,0,0,0))
-	plot(c(0,1),c(0,1), type="n", xlim=c(STmin,STmax), ylim=c(Smin,Smax), axes=FALSE, xaxs="i", yaxs="i",
-			xlab=" ", ylab=" ")
-	arrows(datgrefA$T.Dcm2, datS$X2.5.,datgrefA$T.Dcm2, datS$X97.5.,lwd=2,code=0 )
-	points(datgrefA$T.Dcm2[datS$stand==1], datS$Mean[datS$stand==1],
-			col=colL, pch=19, cex=4)
-	points(datgrefA$T.Dcm2[datS$stand==2], datS$Mean[datS$stand==2],
-			col=colH, pch=19, cex=4)			
-	
-	axis(1, seq(STmin,STmax, by=1), rep(" ", length(seq(STmin,STmax, by=1))), cex.axis=axisC, lwd.ticks=3)
-	mtext(seq(STmin,STmax, by=1),at=seq(STmin,STmax, by=1), line=4, side=1, cex=3)
-	mtext("Organic soil temperature",side=1, line=8, cex=4)
-	
-	if(datparm$Sig[datparm$parms3=="b"&datparm$stand==1&datparm$pN==4]==1){
-		points(STseq,regL(STseq,datparm$Mean[datparm$parms3=="b"&datparm$stand==1&datparm$pN==1],
-				datparm$Mean[datparm$parms3=="b"&datparm$stand==1&datparm$pN==4],5.04), type="l", lwd=4, col=colL)
-	}else{
-		abline(h=datparm$Mean[datparm$parms3=="b"&datparm$stand==1&datparm$pN==1], lwd=4, lty=3, col=colL)
-	}
-			
-	if(datparm$Sig[datparm$parms3=="b"&datparm$stand==2&datparm$pN==4]==1){
-		points(STseq,regL(STseq,datparm$Mean[datparm$parms3=="b"&datparm$stand==2&datparm$pN==1],
-				datparm$Mean[datparm$parms3=="b"&datparm$stand==2&datparm$pN==4],1.74), type="l", lwd=4, col=colH)
-	}else{
-		abline(h=datparm$Mean[datparm$parms3=="b"&datparm$stand==2&datparm$pN==1], lwd=4, lty=3, col=colH)
-	}	
-	
-	
-box(which="plot")
-
 	
 dev.off()	
 
-
-#################################################################
-####check fit                                             #######
-#################################################################
-plot(datgc$g.c,mugc$mean, xlim=c(0,400), ylim=c(0,400))
-fit1 <- lm(mugc$mean~datgc$g.c)
-summary(fit1)
 
 
 #################################################################
@@ -361,41 +297,3 @@ jpeg(paste0(plotDI , "\\weights.jpg"), width=1700, height=1500, units="px", qual
 	mtext("Precipiation lag time", side=1, line=6, cex=4)		
 dev.off()	
 
-
-
-#################################################################
-####make a plot     of gc                                 #######
-#################################################################
-
-
-#look at how many gc observations in stand day
-#stand day11 for low and 51 for high
-
-xl <- .5
-xh <- 1.3
-yl <- 0
-yh <- 210
-colL <-"royalblue"
-colH <- "tomato3"
-dL <-11
-dH <-51
-
-jpeg(paste0(plotDI , "\\gc.jpg"), width=1500, height=1500, units="px", quality=100)
-	par(mai=c(3,3,3,3))
-	plot(c(0,1),c(0,1), type="n", xlim=c(xl,xh), ylim=c(yl,yh), xlab=" ", ylab=" ",
-		xaxs="i", yaxs="i", axes=FALSE)
-	points(datgc$D[datgc$standDay==dL],datgc$g.c[datgc$standDay==dL], pch=19, col=colL, cex=4)	
-	points(datgc$D[datgc$standDay==dH],datgc$g.c[datgc$standDay==dH], pch=19, col=colH, cex=4)
-	points(seq(xl,xh,by=.1), datgrefA$Mean[dL]*(1-(datS$Mean[dL]*log(seq(xl,xh,by=.1)))),
-			lwd=2, type="l", col=colL)
-	points(seq(xl,xh,by=.1), datgrefA$Mean[dH]*(1-(datS$Mean[dH]*log(seq(xl,xh,by=.1)))),
-			lwd=2, type="l", col=colH)
-	axis(1,seq(.5,1.3, by=.1), rep(" ", length(seq(.5,1.3, by=.1))), cex.axis=3, lwd.ticks=3)	
-	mtext(seq(.5,1.3, by=.1), at=seq(.5,1.3, by=.1), side=1, line=3,cex=3)
-	axis(2, seq(0,200, by=50), cex.axis=3, lwd.ticks=3, las=2) 
-	mtext("Canopy stomatal conductance", side=2, cex=4, line=11)
-	mtext(expression(paste("(g"[c],", mmol m"^"-2", "s"^"-1",")")),side=2, line=5, cex=4)
-	mtext("Vapor pressure deficity (D, kPa)", side=1, cex=4, line=7)
-	legend(.55,200, c("low density gc", "high density gc", "low density model", "high density model"),
-			col=c(colL,colH,colL,colH), lwd=c(NA,NA,3,3), pch=c(19,19,NA,NA), cex=3, bty="n")
-dev.off()		
