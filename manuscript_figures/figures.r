caedfe9463f745394211ce4505a1998f296f2fdf
kroppheather <kroppheather@gmail.com>
2018-03-08 11:03:29 -0500

    added soil type and root density plot

--- manuscript_figures/figures.r
+++ manuscript_figures/figures.r
@@ -36,7 +36,7 @@ source("c:\\Users\\hkropp\\Documents\\GitHub\\larix_density_ecohydro\\thaw_depth
 #################################################################
 ####read in datafiles                                     #######
 #################################################################
-
+#### met and transpiration####
 #read in precip data
 datAirP <- read.csv("c:\\Users\\hkropp\\Google Drive\\viperSensor\\airport\\airport.csv")
 
@@ -53,7 +53,16 @@ datPAR <- read.csv("c:\\Users\\hkropp\\Google Drive\\viperSensor\\met\\PAR.QSOS 
 #read in leaf and sapwood area
 datLSA <- read.csv("c:\\Users\\hkropp\\Google Drive\\Viper_Ecohydro\\sapflux_diag\\tables\\treeSummary.csv")
 
+#### soil and root density ####
+#root density
+datRD<-read.csv("c:\\Users\\hkropp\\Google Drive\\root_analysis\\density_fr.csv")
 
+#read in soil profile data 
+datSP<-read.csv("c:\\Users\\hkropp\\Google Drive\\root_analysis\\soil profile all.csv")
+
+
+
+#### vertical roots ####
 #vertical root  data
 datR<-read.csv("c:\\Users\\hkropp\\Google Drive\\root_analysis\\fine_root_out.csv")
 
@@ -62,7 +71,6 @@ datD<-read.csv("c:\\Users\\hkropp\\Google Drive\\root_analysis\\siteDay\\Depth.c
 #read in vertical root data for estimated depth increments
 datE<-read.csv("c:\\Users\\hkropp\\Google Drive\\root_analysis\\siteDay\\rbio_SiteDay.csv")
 datMD<-read.csv("c:\\Users\\hkropp\\Google Drive\\root_analysis\\siteDay\\medDepth.csv")
-
 
 
 
@@ -327,3 +335,211 @@ dev.off()
 ####  figure 5. Soil type root profile                           ####
 #####################################################################
 
+
+datRD$vol<-datRD$dim1*datRD$dim2*(datRD$depth.end-datRD$depth.start)
+datRD$mid.pt<-datRD$depth.start+((datRD$depth.end-datRD$depth.start)/2)
+#calculate densidy in mg/cm3
+datRD$rdens<-(datRD$fine.root.biomass/datRD$vol)*1000
+
+########################################
+###look specifically  by layer type
+
+#get the end of each layer
+dEL<-aggregate(datRD$depth.end, by=list(datRD$soil.type,datRD$location,datRD$rep,datRD$stand),FUN="max")
+dSL<-aggregate(datRD$depth.start, by=list(datRD$soil.type,datRD$location,datRD$rep,datRD$stand),FUN="min")
+#get total root biomass in the layer
+dRB<-aggregate(datRD$fine.root.biomass, by=list(datRD$soil.type,datRD$location,datRD$rep,datRD$stand),FUN="sum")
+#total volume of the layer
+dV<-aggregate(datRD$vol, by=list(datRD$soil.type,datRD$location,datRD$rep,datRD$stand),FUN="sum")
+#add colnames for each table
+colnames(dEL)<-c("soil.type","loc","rep","stand","dEnd")
+colnames(dSL)<-c("soil.type","loc","rep","stand","dStart")
+colnames(dRB)<-c("soil.type","loc","rep","stand","r.bio")
+colnames(dV)<-c("soil.type","loc","rep","stand","vol")
+
+#get density of root biomass
+dRB$r.dens<-dRB$r.bio/dV$vol
+
+#omit the missing measurement
+dRB<-na.omit(dRB)
+#convert from g/cm3 to mg/cm3
+dRB$mgrD<-dRB$r.dens*1000
+
+
+#now average root density across layers
+layR<-aggregate(dRB$mgrD,by=list(dRB$soil.type,dRB$stand),FUN="mean")
+layRSD<-aggregate(dRB$mgrD,by=list(dRB$soil.type,dRB$stand),FUN="sd")
+layRL<-aggregate(dRB$mgrD,by=list(dRB$soil.type,dRB$stand),FUN="length")
+colnames(layR)<-c("soil.type","stand","r.d")
+colnames(layRSD)<-c("soil.type","stand","r.sd")
+colnames(layRL)<-c("soil.type","stand","r.n")
+
+layR$se<-layRSD$r.sd/sqrt(layRL$r.n)
+layR$pseq<-c(5,1,3,6,2,4)
+
+#make a layer ID to use for stats
+layRID<-data.frame(soil.type=layR$soil.type,stand=layR$stand,pseq=layR$pseq)
+dRBj<-join(dRB,layRID, by=c("soil.type","stand"), type="left")
+
+#anova
+modR<-lm(dRBj$r.dens~as.factor(dRBj$pseq))
+#check residuals
+hist(residuals(modR))
+shapiro.test(residuals(modR))
+#not normal and transformation didn't work (code deleted)
+#do kruskal wallice test
+modRkw<-kruskal.test(dRBj$r.dens~as.factor(dRBj$pseq))
+#results are significant so do a pairwise test
+#mann whitney tests
+pair<-matrix(rep(NA,6*6), ncol=6)
+a<-list()
+for(j in 1:6){
+	for(i in 1:6){
+	if(i != j){
+		a<-wilcox.test(dRBj$r.dens[dRBj$pseq==i|dRBj$pseq==j]~as.factor(dRBj$pseq[dRBj$pseq==i|dRBj$pseq==j]))
+		pair[i,j]<-a$p.value
+			}
+	}
+}
+#flag the pairwise diff that are not sig
+pairI<-ifelse(pair<.05,1,0)
+#pairs 3x4,3x5,4x5,5x6 are not sig diff
+#letters are as follows:
+#1 A
+#2 B
+#3 C
+#4 C
+#5 CD
+#6 D
+
+
+
+#make a table
+SigL<-data.frame(pseq=seq(1,6), sL=c("A","B","C","C", "CD", "D"))
+#join to layR table
+layR<-join(layR,SigL, by="pseq", type="left")
+
+#calculate thickness since the data is actually in total depth
+#from surface
+datSP$bmT<-datSP$total.moss-datSP$green.moss.thickness
+datSP$omT<-datSP$organic.depth-datSP$total.moss
+
+
+datG<-na.omit(data.frame(site=datSP$site,thick=datSP$green.moss.thickness))
+datB<-na.omit(data.frame(site=datSP$site,thick=datSP$bmT))
+datO<-na.omit(data.frame(site=datSP$site,thick=datSP$omT))
+
+#now get stats on each layer
+Green<-aggregate(datG$thick, by=list(datG$site),FUN="mean")
+colnames(Green)<-c("stand","thick")
+GreenSD<-aggregate(datG$thick, by=list(datG$site),FUN="sd")
+colnames(GreenSD)<-c("stand","thick.sd")
+GreenL<-aggregate(datG$thick, by=list(datG$site),FUN="length")
+colnames(GreenL)<-c("stand","thick.n")
+Green$se<-GreenSD$thick.sd/sqrt(GreenL$thick.n)
+
+Brown<-aggregate(datB$thick, by=list(datB$site),FUN="mean")
+colnames(Brown)<-c("stand","thick")
+BrownSD<-aggregate(datB$thick, by=list(datB$site),FUN="sd")
+colnames(BrownSD)<-c("stand","thick.sd")
+BrownL<-aggregate(datB$thick, by=list(datB$site),FUN="length")
+colnames(BrownL)<-c("stand","thick.n")
+Brown$se<-BrownSD$thick.sd/sqrt(BrownL$thick.n)
+
+Organic<-aggregate(datO$thick, by=list(datO$site),FUN="mean")
+colnames(Organic)<-c("stand","thick")
+OrganicSD<-aggregate(datO$thick, by=list(datO$site),FUN="sd")
+colnames(OrganicSD)<-c("stand","thick.sd")
+OrganicL<-aggregate(datO$thick, by=list(datO$site),FUN="length")
+colnames(OrganicL)<-c("stand","thick.n")
+Organic$se<-OrganicSD$thick.sd/sqrt(OrganicL$thick.n)
+
+
+
+###########################################################
+#make a barplot 
+#of density in each layer
+#add a plotting order to layR
+lgcx <-2.75
+txcx <- 2.75
+mcx <- 4
+ltcx <- 3
+#order is moss, organic, mineral
+#start with hd in each layer
+lcol <- "grey50"
+hcol <- "grey75"
+layR$pcol<-rep(c(hcol,lcol), each=3)
+mgcol <- "grey85"
+mbcol <- "grey50"
+ocol <- "grey30"
+
+lda <- 3
+#setup plot layout
+wd<-18
+hd<-18
+jpeg(paste0(plotDI,"\\root_type.jpg"), width=1600, height=1050, units="px",quality=100)
+ac<-layout(matrix(seq(1,2),ncol=2), width=rep(lcm(wd),2),height=rep(lcm(hd),2))
+layout.show(ac)
+
+par(mai=c(0,0,0,0))
+plot(c(0,1),c(0,1), type="n",ylim=c(6.1,-.1),xlim=c(-.1,8), axes=FALSE, xlab=" ", ylab=" ", yaxs="i",
+		xaxs="i")
+	for(i in 1:dim(layR)[1]){
+	polygon(c(0,layR$r.d[i],layR$r.d[i],0),c(layR$pseq[i]-1,layR$pseq[i]-1,layR$pseq[i],layR$pseq[i]),
+			col=layR$pcol[i])	
+	}
+	arrows(layR$r.d-layR$se,layR$pseq-.5,layR$r.d+layR$se,layR$pseq-.5,code=0,lwd=lda )
+	
+mtext(c("moss", "organic", "mineral", "< 20 cm"), at =c(1,3,5,5.5),side=2,line=2.5, las=2, cex=ltcx)
+axis(2, c(1,3,5), c(" ", " ", " "), las=2, lwd.ticks=3)
+axis(1, seq(0,7, by=1),rep(" ", length(seq(0,7, by=1))), lwd.ticks=3)
+mtext(seq(0,7, by=1), at=seq(0,7, by=1), line=2.5,side=1,cex=ltcx)
+
+
+box(which="plot")
+legend(3,0,c("high density","low density"), fill=c(hcol,lcol),bty="n", cex=lgcx)
+mtext("Soil layer type", side=2, cex=mcx, line=13)
+mtext(expression(paste("Root biomass mg cm"^"-3")), side=1, cex=mcx, line=8)
+text(layR$r.d+layR$se+.5,layR$pseq-.5,layR$sL,cex=txcx)
+#plot thickness
+par(mai=c(0,0,0,0))
+plot(c(0,1),c(0,1), type="n",ylim=c(20,0),xlim=c(0,2), axes=FALSE, xlab=" ", ylab=" ", yaxs="i",
+		xaxs="i")
+	for(i in 1:2){
+		polygon(c(i-1,i-1,i,i),c(0,Green$thick[i],Green$thick[i],0), col=mgcol)
+	}
+
+	for(i in 1:2){
+		polygon(c(i-1,i-1,i,i),
+		c(Green$thick[i],Green$thick[i]+Brown$thick[i],Green$thick[i]+Brown$thick[i],Green$thick[i]), 
+		col=mbcol)
+	}
+
+		for(i in 1:2){
+		polygon(c(i-1,i-1,i,i),
+		c(Green$thick[i]+Brown$thick[i],Green$thick[i]+Brown$thick[i]+Organic$thick[i],
+		Green$thick[i]+Brown$thick[i]+Organic$thick[i],Green$thick[i]+Brown$thick[i]), 
+		col=ocol)
+	}
+
+	
+	
+		arrows(c(.5,1.5), Green$thick-Green$se,c(.5,1.5), Green$thick+Green$se, lwd=lda , code=0)
+		
+			arrows(c(.5,1.5), Green$thick+Brown$thick-Brown$se,c(.5,1.5), 
+					Green$thick+Brown$thick+Brown$se, lwd=lda , code=0)
+		arrows(c(.5,1.5), Green$thick+Brown$thick+Organic$thick-Organic$se,c(.5,1.5), 
+					Green$thick+Brown$thick+Organic$thick+Organic$se, lwd=lda , code=0)
+	
+	axis(4,seq(20,0, by=-2),rep(" ", length(seq(20,0, by=-2))), lwd.ticks=3)
+	mtext(seq(20,0, by=-2), at=seq(20,0, by=-2), line=2.5,side=4,las=2,cex=ltcx)
+	
+	box(which="plot")	
+	axis(1, c(.5,1.5), c(" ", " "), lwd.ticks=3)
+	mtext( c("high density", "low density"), at=c(.5,1.5), line=2.5,side=1,cex=ltcx)
+	
+mtext("Soil layer depth (cm)", side=4, cex=mcx, line=10)
+mtext("Stand", side=1, cex=mcx, line=8)
+
+legend(.92,13,c("green moss", "brown moss", "fibric organic"), fill=c(mgcol, mbcol,ocol), bty="n", cex=lgcx)
+dev.off()
